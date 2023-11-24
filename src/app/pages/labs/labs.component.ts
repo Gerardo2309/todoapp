10bdf788a9e9250ebfe3d2b0a4361a61842a0337
GeRa#3C <gerardocordova15@gmail.com>
2023-11-24 14:24:55 -0500

    clase 4 inputs

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = 'Hola soy una variable declarada desde el componente';
+  tasks = [
+    'Instalar angular',
+    'Crear Proyecto',
+    'Crear Componentes'
+  ];
+
+  name = "Gerardo";
+  age = 23;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png' ;
+
+  person = {
+    name: 'pedro',
+    age: 25,
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  }
+
+  clcickhandler(){
+    alert('Hola Gracias por hacer click')
+  }
+}
