d8e297f0df159341fdab8864252d47a5d16d90a3
GeRa#3C <gerardocordova15@gmail.com>
2023-11-29 21:56:48 -0500

    ejercicio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Task} from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -10,18 +11,44 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
 
-  tasks = signal([
-    'Instalar angular',
-    'Crear Proyecto',
-    'Crear Componentes',
-    'Crear aplicacion de tareas'
-  ]);
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Instalar angular',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear Proyecto',
+      completed: true
+    }
+    ,
+    {
+      id: Date.now(),
+      title: 'Crear Componentes',
+      completed: false
+    }
+    ,
+    {
+      id: Date.now(),
+      title: 'Crear aplicacion de tareas',
+      completed: false
+    }]);
 
   changeHandler(event: Event){
     const input = event.target as HTMLInputElement;
     const newtask =  input.value;
-    this.tasks.update((tasks) => [...tasks, newtask]);
+    this.addTask(newtask);
   }
+
+addTask(title: string){
+  const newtask = {
+    id: Date.now(),
+    title,
+    completed: false
+  }
+  this.tasks.update((tasks) => [...tasks, newtask]);
+}
 
   deletTask (index: number){
 this.tasks.update((tasks) => tasks.filter((task,position) => position !== index));
